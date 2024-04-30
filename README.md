645b7d665280b5f22a3d19b5119eab76a99238ae
Edd Turtle <edd@designedbyaturtle.com>
2024-04-30 17:48:34 +0100

    Hello World :star:

---
+++ README.md
@@ -0,0 +1,37 @@
+# Buggy-Code (Slim Framework 4 Skeleton Application)
+
+This is a simple web application built on the Slim Framework.
+
+### Install
+
+```bash
+git clone x
+```
+
+```bash
+cd my-simple-app
+```
+
+```bash
+composer install
+```
+
+### How to Run
+
+To run the application in development, you can run these commands 
+
+```bash
+composer start
+```
+
+Or you can use `docker-compose` to run the app with `docker`, so you can run these commands:
+
+```bash
+cd my-simple-app
+```
+
+```bash
+docker-compose up -d
+```
+
+After that, open `http://localhost:8080` in your browser.
---
+++ app/routes.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->get('/', function (Request $request, Response $response) {
+
+        // Challenge 1. This should not print anything out.
+        // It's suppose to only print out if var1 is hoowla and var2 is marvelous.
+        // Q: Why is it printing? What can we do to prevent it printing, while keeping the same lookup criteria?
+        $var1 = 'hoowla'; 
+        $var2 = 'marvelous';
+        if ($var1 === 'hoowla' || $var1 === 'buzz' && $var2 === 'irritating') {
+            $response->getBody()->write('Do not print (1)');
+        }
+
+        // Challenge 2. This should not print anything out.
+        // As $testVar is not equal it shouldn't print out, so why is it?
+        // Q: Why is it printing?
+        $testVar = 'Something';
+        if ($testVar = 'Else') {
+            $response->getBody()->write('Do not print (2)');
+        }
+
+        // Challenge 3: Can we return the response as JSON?
+        $response->getBody()->write('All Complete!');
+
+        return $response;
+    });
+
+
+};
